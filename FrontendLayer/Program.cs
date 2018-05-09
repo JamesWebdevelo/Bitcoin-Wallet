4cd8494682c73fa33b9ad76d371e9835696a75f1
Fabrizio <Fabrizio_39@hotmail.com>
2018-05-09 08:46:20 +0200

    Add project files.

---
+++ FrontendLayer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+//https://youtu.be/nuM6AojRFHk?t=5m11s
+namespace FrontendLayer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseElectron(args) // Builder extension
+                .Build();
+    }
+}
